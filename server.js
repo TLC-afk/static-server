5c928ba290992a4e6736223c7532b0877869077f
Mr.tang <1261651023@qq.com>
2021-08-03 10:56:19 +0800

    second

--- server.js
+++ server.js
@@ -18,34 +18,58 @@ var server = http.createServer(function(request, response){
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
-
+ 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
-  
+  if(path === '/register' && method === 'POST'){
+    response.setHeader('Content-Type','text/html;charset=utf-8')
+    const userArray = JSON.parse(fs.readFileSync('./db/user.json'))
+    const array =[]
+    request.on('data',(chunk) => {
+      array.push(chunk)
+    })
+    request.on('end',() => {
+      
+      const string = Buffer.concat(array). toString()
+      console.log(string)
+      const obj = JSON.parse(string)
+      const lastUser = userArray[userArray.length-1]
+      const newUser = {
+        id: lastUser ? lastUser.id+1 :1,
+        name:obj.name,
+        password:obj.password
+      }  
+      userArray.push(newUser)
+      fs.writeFileSync('./db/user.json',JSON.stringify(userArray))
+    })
+  }else{ 
     response.statusCode = 200
-     //默认主页
-    const filePath = path === '/' ? 'index.html' : path
-    const index =  filePath.lastIndexOf('.')
-    //suffix后缀
-    const suffix = filePath.substring(index)
-    const fileType = {
-      '.html':'text/html',
-      '.css':'text/css',
-      '.js':'text/js',
-      '.png':'image/png',
-      '.jpg':'image/jpeg'
-    }
-    response.setHeader('Content-Type', `${fileType[suffix]};charset=utf-8`)
-   
-    let content 
-    try {
-      content = fs.readFileSync(`./public/${filePath}`)
-    }catch(error){
-      content = '你访问的文件不存在'
-      response.statusCode = 404
-    }
-    response.write(content)
-    response.end()
+    //默认主页
+   const filePath = path === '/' ? 'index.html' : path
+   const index =  filePath.lastIndexOf('.')
+   //suffix后缀
+   const suffix = filePath.substring(index)
+   const fileType = {
+     '.html':'text/html',
+     '.css':'text/css',
+     '.js':'text/js',
+     '.png':'image/png',
+     '.jpg':'image/jpeg'
+   }
+   response.setHeader('Content-Type', `${fileType[suffix]};charset=utf-8`)
+  
+   let content 
+   try {
+     content = fs.readFileSync(`./public/${filePath}`)
+   }catch(error){
+     content = '你访问的文件不存在'
+     response.statusCode = 404
+   }
+   response.write(content)
+   response.end()
+  }
+  
+    
   
 
   /******** 代码结束，下面不要看 ************/
